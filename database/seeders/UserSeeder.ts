ff14f326f254b41f3eaa1cb2bff09a32e691b836
fajar-dev <fajarrivaldi2015@gmail.com>
2024-08-25 15:46:35 +0700

    refac: user seeder

--- database/seeders/UserSeeder.ts
+++ database/seeders/UserSeeder.ts
@@ -1,23 +1,20 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import Role from 'App/Models/Role'
 import User from 'App/Models/User'
 
 export default class UserSeedeer extends BaseSeeder {
   public async run() {
-    const superadminRole = await Role.query().where('name', 'Superdmin').firstOrFail()
-    const adminRole = await Role.query().where('name', 'Admin').firstOrFail()
     const users = [
       {
         name: 'Super Admin',
         email: 'superadmin@example.com',
         password: 'password',
-        role_id: superadminRole.id,
+        role: 'superadmin',
       },
       {
         name: 'Admin',
         email: 'admin@example.com',
         password: 'password',
-        role_id: adminRole.id,
+        role: 'admin',
       },
     ]
 
