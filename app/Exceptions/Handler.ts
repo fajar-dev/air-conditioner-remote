dc95c3f43c852e9ef1edeb0ffcf0ab783186deb0
fajar-dev <fajarrivaldi2015@gmail.com>
2024-08-29 13:13:36 +0700

    feat: handle error exceptions

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -15,6 +15,8 @@
 
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import ApiResponse from 'App/Helpers/ApiResponse'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   protected statusPages = {
@@ -25,5 +27,36 @@ export default class ExceptionHandler extends HttpExceptionHandler {
 
   constructor() {
     super(Logger)
+  }
+
+  public async handle(error: any, ctx: HttpContextContract) {
+    // Handle errors based on route prefix
+    if (ctx.request.url().startsWith('/api')) {
+      return this.handleApiErrors(error, ctx)
+    }
+    // For web routes, use the default handler
+    return super.handle(error, ctx)
+  }
+
+  private async handleApiErrors(error: any, ctx: HttpContextContract) {
+    if (error.code === 'E_VALIDATION_FAILURE') {
+      return ApiResponse.validationError(ctx.response, error.messages.errors)
+    }
+
+    if (error.code === 'E_ROUTE_NOT_FOUND') {
+      return ApiResponse.notFound(ctx.response, error.message)
+    }
+
+    if (error.code === 'E_INVALID_API_TOKEN') {
+      return ApiResponse.unauthorized(ctx.response, error.message)
+    }
+
+    if (error.code === 'E_UNAUTHORIZED_ACCESS') {
+      return ApiResponse.unauthorized(ctx.response, error.message)
+    }
+    /**
+     * Forward rest of the exceptions to the parent class
+     */
+    return ApiResponse.internalServerError(ctx.response, error.message, error.stack)
   }
 }
