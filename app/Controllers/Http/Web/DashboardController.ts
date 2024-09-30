a1611f85ee9892d260749d65782b8a24633c9644
fajar-dev <fajarrivaldi2015@gmail.com>
2024-09-30 14:13:09 +0700

    fix: fixed dashboard

--- app/Controllers/Http/Web/DashboardController.ts
+++ app/Controllers/Http/Web/DashboardController.ts
@@ -2,6 +2,20 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class DashboardController {
   public async index({ view }: HttpContextContract) {
-    return view.render('pages/dashboard')
+    const now = new Date()
+    const hour = now.getHours()
+
+    let great = ''
+
+    if (hour >= 5 && hour < 12) {
+      great = 'Good Morning'
+    } else if (hour >= 12 && hour < 17) {
+      great = 'Good Afternoon'
+    } else if (hour >= 17 && hour < 21) {
+      great = 'Good Evening'
+    } else {
+      great = 'Good Night'
+    }
+    return view.render('pages/dashboard', { great })
   }
 }
